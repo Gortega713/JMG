c4204e7c571499203718ecacd83d74191958ce53
Unknown <jpardo-cano729@west-mec.org>
2018-03-12 10:39:23 -0700

    Index update
    Implamented a animated nav icon and also a nav bar that comes into view form the left hand side it has some bugs i want to find to make the flow better

---
+++ html5-boilerplate_v6.0.1/js/main.js
@@ -0,0 +1,15 @@
+function navBar(animate, ){
+    animate.classList.toggle("change");
+}
+
+function openNav(){
+    document.getElementById("Sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav(){
+    document.getElementById("Sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor ="white";
+}
