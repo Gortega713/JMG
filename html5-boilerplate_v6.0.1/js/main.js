627717880c7b5119311f9eed04678d59e157e59c
Unknown <jpardo-cano729@west-mec.org>
2018-05-02 18:13:45 -0700

    Started local storage setup
    Set up a validation set to check the user computer to check for previous sign-up attempts

--- html5-boilerplate_v6.0.1/js/main.js
+++ html5-boilerplate_v6.0.1/js/main.js
@@ -1,8 +1,48 @@
 window.addEventListener("load", function(){
-    document.getElementById("email").onblur = 
-
+   
+   checkLogin();    
 });
 
-function displayInfo(){
-    localStorage.setItem("email",)
+// function storeValues(){
+//     if(localStorage.getItem("email") == ""){
+//     var userName = document.getElementById("email");
+//     localStorage.setItem("email", "asdfg");
+//     }
+//     else{
+//         checkLogin();
+//     }
+//     // var userPass = document.getElementById("password");
+//     // localStorage.setItem("Password", userPass.value);
+//     checkLogin();
+// }
+
+function checkLogin() {
+     var storedEmail = localStorage.getItem("email");
+     var storedPass = localStorage.getItem("Password");
+
+     var userName = document.getElementById("email");
+     var password = document.getElementById("password").value;  
+     document.getElementById("submit").addEventListener("click",function(){
+        if(document.getElementById("popUp").classList.contains("show")){
+            if(userName.value == localStorage.getItem("email")){
+            document.getElementById("popUp").classList.remove("show");
+            }
+            if(document.getElementById("popUp").classList.contains("shake")){
+                document.getElementById("popUp").classList.remove("shake");
+            }
+            if(userName.value !== localStorage.getItem("email")){
+                document.getElementById("popUp").classList.add("shake");
+            }
+        }
+        if(userName.value !== localStorage.getItem("email")){
+            document.getElementById("popUp").classList.add("show");
+            
+        }
+        if(password.value !== localStorage.getItem("Password")){
+            document.getElementById("popUp2").classList.add("show")
+        }
+    });
 }
+
+
+
